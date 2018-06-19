8b9d4f01f5975c6afe5835c14ad676b001bca8ee
slimerence <39113678+slimerence@users.noreply.github.com>
2018-06-19 17:38:06 +1000

    init

---
+++ Maydriver/layouts/frontend/_nav.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            Vitality
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
+            Menu
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Story</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Blog</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
