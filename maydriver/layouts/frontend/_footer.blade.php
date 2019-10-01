00be9d7691fa2c621fb322112b8e0a62f4641675
slimerence <39113678+slimerence@users.noreply.github.com>
2019-10-02 09:55:44 +1000

    Update _footer.blade.php

--- maydriver/layouts/frontend/_footer.blade.php
+++ maydriver/layouts/frontend/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer style="background-color: #353537;">
     <div class="container text-center">
-        <p class="copyright" style="color: #efdab9;">&copy; 2018 Sofardriving All Rights Reserved. Powered By <a href="https://www.webmelbourne.com/" target="_blank">WebMelbourne</a></p>
+        <p class="copyright" style="color: #efdab9;">&copy; 2019 Sofardriving All Rights Reserved. Powered By <a href="https://kongfuseo.com.au/" target="_blank">Kongfuseo</a></p>
     </div>
 </footer>
