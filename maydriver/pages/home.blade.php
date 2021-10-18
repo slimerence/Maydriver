1ccb8c99d65350e4a9940c7313bb74ab35d71e6c
LIHANGT <lihangt@student.unimelb.edu.au>
2021-10-18 20:45:33 +1100

    add another service

--- maydriver/pages/home.blade.php
+++ maydriver/pages/home.blade.php
@@ -136,6 +136,47 @@
         </div>
     </section>
 
+    <section class="page-section bg-light" id="services">
+        <div class="container">
+            <div class="wow fadeIn text-center">
+                <h2>避免踩坑</h2>
+            </div>
+            <hr class="colored">
+            <div class="row text-center">
+                <div class="col">
+                    <div class="wow fadeIn px-4 pb-4 pb-lg-0 " data-wow-delay=".2s">
+                        <i class="fa fa-language fa-2x"></i>
+                        <p class="mb-0">标准普通话/英语双语教学</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wow fadeIn px-4 pb-4 pb-lg-0 " data-wow-delay=".2s">
+                        <i class="fa fa-taxi fa-2x"></i>
+                        <p class="mb-0">双刹车双油门自动挡专业教练车，车身轻巧，操控简易</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wow fadeIn px-4 " data-wow-delay=".8s">
+                        <i class="fa fa-book fa-2x"></i>
+                        <p class="mb-0">零基础学员手脚协调专业训练及结合《Vicroads独立驾驶之路》图文详解及教授交规</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wow fadeIn px-4 pb-4 pb-lg-0" data-wow-delay=".4s">
+                        <i class="fa fa-globe fa-2x"></i>
+                        <p class="mb-0">海外驾照陪练及结合《Vicroads独立驾驶之路》图文详解及教授交规</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wow fadeIn px-4 pb-4 pb-lg-0 " data-wow-delay=".6s">
+                        <i class="fa fa-car fa-2x"></i>
+                        <p class="mb-0">路考集训：熟悉和操练路局路考路线，模拟路考模式，图片实景解析难点路口及注意事项</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!-- Call to Action -->
     <section class="call-to-action" style="background-image: url({{ asset('images/backgrounds/footer-v1.jpg') }});">
         <div class="container wow fadeIn">
